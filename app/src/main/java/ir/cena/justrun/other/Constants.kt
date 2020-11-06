afa3140a62b481d066ee65f538eb08d23e6e1deb
Cena <cena.tabriziyan@gmail.com>
2020-11-06 18:57:52 +0330

    Impl services functionality

--- app/src/main/java/ir/cena/justrun/other/Constants.kt
+++ app/src/main/java/ir/cena/justrun/other/Constants.kt
@@ -7,6 +7,13 @@ object Constants {
     const val RUNNING_DATABASE_NAME = "running_db"
     const val REQUEST_CODE_LOCATION_PERMISSION = 0
 
+    /**
+     * For services
+     */
+    const val ACTION_START_OR_RESUME_SERVICE = "ACTION_START_OR_RESUME_SERVICE"
+    const val ACTION_PAUSE_SERVICE = "ACTION_PAUSE_SERVICE"
+    const val ACTION_STOP_SERVICE = "ACTION_STOP_SERVICE"
+
 
     fun View.gone() {
         this.visibility = View.GONE
--- app/src/main/java/ir/cena/justrun/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/ir/cena/justrun/ui/fragments/TrackingFragment.kt
@@ -1,5 +1,6 @@
 package ir.cena.justrun.ui.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
@@ -7,6 +8,8 @@ import androidx.fragment.app.viewModels
 import com.google.android.gms.maps.GoogleMap
 import dagger.hilt.android.AndroidEntryPoint
 import ir.cena.justrun.R
+import ir.cena.justrun.other.Constants.ACTION_START_OR_RESUME_SERVICE
+import ir.cena.justrun.services.TrackingService
 import ir.cena.justrun.ui.viewmodel.MainViewModel
 import kotlinx.android.synthetic.main.frgment_tracking.*
 
@@ -24,7 +27,21 @@ class TrackingFragment : Fragment(R.layout.frgment_tracking) {
          */
         mapView.onCreate(savedInstanceState)
 
+
+        btnToggleRun.setOnClickListener {
+            sendCommandToService(ACTION_START_OR_RESUME_SERVICE)
+        }
         mapView.getMapAsync { map = it }
+    }
+
+
+    private fun sendCommandToService(action: String) {
+
+        Intent(requireContext(), TrackingService::class.java).also {
+            it.action = action
+            requireContext().startService(it)
+        }
+
     }
 
     override fun onStart() {
